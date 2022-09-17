2af251b53ea65158f83b4a53f7d6287f649ee959
Vinicius Pereira <m.vini846@gmail.com>
2022-09-17 16:27:23 -0300

    creating dockerfile

--- BACK/src/infra/database/PgPromiseConnection.ts
+++ BACK/src/infra/database/PgPromiseConnection.ts
@@ -5,7 +5,7 @@ export default class PgPromiseConnection implements Connection {
 	connection: any;
 
 	constructor () {
-		this.connection = pgp()("postgres://postgres:Postgres2022@localhost:5432/app"); // rever
+		this.connection = pgp()("postgres://postgres:Postgres2022@localhost:5432/app"); // todo
 	}
 
 	query(statement: string, params: any): Promise<any> {
